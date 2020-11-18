887fe2e5c769de5a76f198e13eaa07173571ca22
nguyendinhthuan <nguyendinhthuan1698@gmail.com>
2020-11-18 19:05:29 +0700

    commit

--- BaiTapLon_QuanLyLinhKien/Model/clsNhanVien.cs
+++ BaiTapLon_QuanLyLinhKien/Model/clsNhanVien.cs
@@ -8,5 +8,46 @@ namespace BaiTapLon_QuanLyLinhKien.Model
 {
     class clsNhanVien
     {
+        private string maNhanVien;
+        private string tenNhanVien;
+        private string diaChi;
+        private string soDienThoai;
+
+        public string MaNhanVien { get => maNhanVien;
+            set
+            {
+                if(value == "")
+                {
+                    throw new Exception("Phải nhập mã");
+                }
+                else
+                {
+                    maNhanVien = value;
+                }
+            }
+        }
+        public string TenNhanVien { get => tenNhanVien; set => tenNhanVien = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+        public string SoDienThoai { get => soDienThoai; set => soDienThoai = value; }
+
+        public override bool Equals(object obj)
+        {
+            clsNhanVien objNhanVien = (clsNhanVien)obj;
+            return this.MaNhanVien.Equals(objNhanVien.maNhanVien);
+        }
+
+        public clsNhanVien(string maNhanVien, string tenNhanVien, string diaChi, string soDienThoai)
+        {
+            this.MaNhanVien = maNhanVien;
+            this.TenNhanVien = tenNhanVien;
+            this.DiaChi = diaChi;
+            this.SoDienThoai = soDienThoai;
+        }
+
+        public clsNhanVien()
+        {
+
+        }
+
     }
 }
